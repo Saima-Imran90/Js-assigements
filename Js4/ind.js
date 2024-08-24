241fc685ac827d8400ffce2dec8669f08b656436
Saima Imran <smalldeeds1234@gmail.com>
2024-08-24 21:10:48 +0500

    Add files via upload

---
+++ Js4/ind.js
@@ -0,0 +1,33 @@
+
+// document.write("My birth year is 1990 <br/> Data type of my declared variable is number")
+
+
+var num=1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+num=num+1;
+alert("You have visited my site "+num+" times")
+
